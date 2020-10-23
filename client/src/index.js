527f4bb997fb4426fb5c631174cc6fa3ed2643d0
andre <andrebertonhadev@gmail.com>
2020-10-23 12:10:59 -0700

    update: added client folder to build the frontend of the projectg

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
+
+const client = new ApolloClient();
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/edit/:id' component={Edit} />
+      <Route path='/create' component={Create} />
+      <Route path='/show/:id' component={Show} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
