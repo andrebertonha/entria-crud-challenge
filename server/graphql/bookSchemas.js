03c462b44eadbf15c4d92fb1eac1cb4f30b593f3
andre <andrebertonhadev@gmail.com>
2020-10-23 06:54:10 -0700

    update: addded methods to the bookSchema.js

--- server/graphql/bookSchemas.js
+++ server/graphql/bookSchemas.js
@@ -75,4 +75,92 @@ var queryType = new GraphQLObjectType({
     }
 });
 
-module.exports = new GraphQLSchema({query: queryType});
+var mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: function () {
+        return {
+            addBook: {
+                type: bookType,
+                args: {
+                    isbn: {
+                        type new GraphQLNonNull(GraphQLString)
+                    },
+                    title: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    author: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    description: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    published_year: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    publisher: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve: function (root, params) {
+                    const bookModel = new BookModel(params);
+                    const newBook = bookModel.save();
+                    if(!newBook) {
+                        throw new Error('Error');
+                    }
+                    return newBook
+                }
+            },
+            updateBook: {
+                type: bookType,
+                args: {
+                    id: {
+                        name: 'id',
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    isbn: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    title: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    author: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    description: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    published_year: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    publisher: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve(root, params) {
+                    return BookModel.findByIdAndUpdate(params.id, { isbn: params.isbn, title: params.title, author: params.author,
+                    description: params.description, published_year: params.published_year, publisher: params.publisher, 
+                    updated_date: new Date() }, function(err){
+                        if(err) return next(err);
+                    });
+                }
+            },
+            removeBook: {
+                type: bookType,
+                args: {
+                    id: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve(root, params) {
+                    const remBook = BookModel.findByIdAndRemove(params.id).exec();
+                    if(!remBook) {
+                        throw new Error('Error')
+                    }
+                    return remBook
+                }
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({ query: queryType, mutation: mutation });
