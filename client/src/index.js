ec6fe05be6da2146be83342ce4060cd6b199aa20
andre <andrebertonhadev@gmail.com>
2020-10-24 14:59:25 -0700

    app fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 import Edit from './components/Edit';
 import Create from './components/Create';
 import Show from './components/Show';
 
-const client = new ApolloClient();
+const client = new ApolloClient({ uri: 'http://localhost:3000/graphql' });
 
-  ReactDOM.render(
+ReactDOM.render(
     <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-    <Router>
-      <div>
-        <Route exact path='/' component={App} />
-        <Route path='/edit/:id' component={Edit} />
-        <Route path='/create' component={Create} />
-        <Route path='/show/:id' component={Show} />
-      </div>
-    </Router>,
-    document.getElementById("root")
-  );
-
-reportWebVitals();
+        <Router>
+            <div>
+                <Route exact path='/' component={App} />
+                <Route path='/edit/:id' component={Edit} />
+                <Route path='/create' component={Create} />
+                <Route path='/show/:id' component={Show} />
+            </div>
+        </Router>
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
